d28ae3f447b46c6b80b55aed919a4fa0503113c6
Rupa Kanchi <RupaSKanchi@gmail.com>
2022-04-03 18:26:43 -0500

    update dashboard using yml for docs

---
+++ docs/shell.sh
@@ -0,0 +1,10 @@
+git init
+git add *
+git commit -m "update dashboard using yml for docs"
+
+# use one the these based on whatever the primary branch is called in your repo
+git push -u master
+or
+git push -u main
+or 
+git push -u origin
